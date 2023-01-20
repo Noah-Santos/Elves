a8fd1708c278a9d043cd565a91d3062e75caddcf
NoahSantos <nsanto591@west-mec.org>
2023-01-19 18:14:48 -0700

    Part2?
    Got a number for part 2 but don't know if it is correct.

--- index.js
+++ index.js
@@ -24,16 +24,42 @@ class Box{
         // gets min and max and creates property for it
         this.small = Math.min(this.l, this.w, this.h);
         this.large = Math.max(this.l, this.w, this.h);
+        this.medium = 0;
+        // console.log(this.small);
+        // console.log(this.large);
         // looks to see if width is middle value
         // if(this.l != this.small && this.l != this.large){
-        //     this.medium = Number(this.l);
+        //     this.medium = this.l;
+            
+        // }else if(this.w != this.small && this.w != this.large){
+        //     this.medium = this.w;
+        // }else if(this.h != this.small && this.h != this.large){
+        //     this.medium = this.h;
         // }
-        // else if(this.w != this.small && this.w != this.large){
-        //     this.medium = Number(this.w);
-        // }
-        // else if(this.h != this.small && this.h != this.large){
-        //     this.medium = Number(this.h);
-        // }
+        if(this.medium == 0){
+            if((this.l == this.w || this.l == this.h)){
+                if(this.l == this.small){
+                    this.medium = this.small;
+                }else{
+                    this.medium = this.large;
+                }
+            }else if((this.w == this.l || this.w == this.h)){
+                if(this.w == this.small){
+                    this.medium = this.small;
+                }else{
+                    this.medium = this.large;
+                }
+            }else if((this.h == this.l || this.h == this.w)){
+                if(this.h == this.small){
+                    this.medium = this.small;
+                }else{
+                    this.medium = this.large;
+                }
+            }
+        }
+        console.log(this.small)
+        console.log(this.large)
+        console.log(this.medium);
     }
 }
 // creates array to hold objects and 2 variables to hold volume and surface area
